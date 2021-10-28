0caab60b57b24026b72a9b88edec554b87394da4
cheese_ge <69394849+cheesege@users.noreply.github.com>
2021-10-28 23:33:11 +0800

    first commit

---
+++ 1-1.cpp
@@ -0,0 +1,18 @@
+// 引入標準程式庫中相關的輸入、輸出程式
+#include <iostream>
+// 引入標準程式庫中相關的字串程式
+
+// std 為標準程式庫的命名空間
+using namespace std;
+
+int main() {
+	cout << "Hello World!";
+	cout << endl;
+	cout << "what your name? ";
+	string a="";
+	string b("Hi ");
+	cin >> a;
+	cout << b << a ;
+	return 0;
+}
+//執行exe, .\ exe檔案名 
