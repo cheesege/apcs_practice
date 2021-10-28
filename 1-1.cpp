3ee3affb9d1ada773c57980882a8159ebf69921f
cheese_ge <69394849+cheesege@users.noreply.github.com>
2021-10-29 00:32:52 +0800

    Update

--- 1-1.cpp
+++ 1-1.cpp
@@ -1,4 +1,6 @@
-// 引入標準程式庫中相關的輸入、輸出程式
+/**
+ * 引入標準程式庫中相關的輸入、輸出程式
+ */
 #include <iostream>
 // 引入標準程式庫中相關的字串程式
 
@@ -6,13 +8,13 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!";
-	cout << endl;
-	cout << "what your name? ";
-	string a="";
-	string b("Hi ");
-	cin >> a;
-	cout << b << a ;
-	return 0;
+    cout << "Hello World!";
+    cout << endl;
+    cout << "what your name? ";
+    string a;
+    string b("Hi ");
+    cin >> a;
+    cout << b << a;
+    return 0;
 }
-//執行exe, .\ exe檔案名 
+// 執行exe, .\ exe檔案名
