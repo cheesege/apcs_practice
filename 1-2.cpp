69b81d9091aad5cd845a6165163897697b05fdf7
cheese_ge <69394849+cheesege@users.noreply.github.com>
2021-10-28 23:39:01 +0800

    add

---
+++ 1-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+string list1[] = {"A","B","C","D","E"};
+cout << list1[2];
+int b = 3;
+b*=3;
+cout << b;
+
+
+
+
+}
+
+void foo() {
+	cout << "bar\n";
+}
---
+++ d049.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    a -= 1911;
+    cout << a
+}
