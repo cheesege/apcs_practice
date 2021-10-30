bd1e171d2ac6f8730d75c647c0376235466d130f
cheese_ge <69394849+cheesege@users.noreply.github.com>
2021-10-30 20:50:20 +0800

    Update d051.cpp

--- d051.cpp
+++ d051.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     double a;
     cin >> a;
-    cout <<  fixed << setprecision(3) << (a-32)*5/9+0.000;
+    cout <<  fixed << setprecision(3) << (a-32)*5/9;
     //fixed 為固定小數點位數
     //setprecision 為指定輸出位數
     //fixed << setprecision(X) 為指定小數點位數
